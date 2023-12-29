25a419542404d20cbcda06e5c33d5c1ec67ce6f8
Abel Daniel <abelonah@yahoo.com>
2023-12-29 13:36:05 +0100

    Starting files

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+
+
+
+
+
+
+
+
+app.listen(port, (req, res)=>{
+    console.log(`Your server is running in port ${port}`);
+})
