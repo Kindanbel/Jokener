535e9b92d90c0b165d7cf61523f029ecccfeb88d
Abel Daniel <abelonah@yahoo.com>
2023-12-31 01:28:33 +0100

    Putting front-end on server

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from "express";
-import axios from "axios";
 import bodyParser from "body-parser";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}))
+
+
 
 
 app.get("/", (req, res)=>{
